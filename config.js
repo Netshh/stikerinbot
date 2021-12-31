7d0d7849abe0c3cdc4277a2c7a77ff9dc0a4dd5e
Netshh <93586570+Netshh@users.noreply.github.com>
2021-12-31 20:06:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 let fs = require('fs')
-global.owner = ['6289662643883'] // Letakan nomor kamu disini
+global.owner = ['6289662643883','6281267855223'] // Letakan nomor kamu disini
 global.mods = [] // Moderator?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Pengguna premium tidak memerlukan limit
 global.APIs = { // API Prefix
